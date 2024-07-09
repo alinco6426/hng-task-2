a33c79044ea04850b4ef5b57ee9f5c3c56e1326f
adegbenro marvellous <alinco2319@gmail.com>
2024-07-09 18:27:46 +0100

    updated

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,4 +1,5 @@
 import "../styles/product.css"
+import iconStar from "../assets/icons/icon-star.svg"
 
 function Product ({product}){
      return(
@@ -7,8 +8,15 @@ function Product ({product}){
           <img src={product.image} alt="product-image" />
         </figure>
         <div className="product-details-container">
+
+          <div className="icon-stars-container">
+          {Array(5).fill().map((_, index) => (
+            <img src={iconStar} alt="icon-star" className="icon-star" key={index} />
+          ))}
+        </div>
+        
           <h5 className="product-name">{product.name}</h5>
-          <h2 className="product-price">{product.price.toLocaleString()}</h2>
+          <h2 className="product-price"># {product.price.toLocaleString()}</h2>
           <button className="add-to-cart-button">+</button>
         </div>
       </section>
