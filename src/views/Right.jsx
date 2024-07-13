46594f2817b12d2b7f5b395e87aa4ff2e0936e63
adegbenro marvellous <alinco2319@gmail.com>
2024-07-13 02:27:18 +0100

    axios added

--- src/views/Right.jsx
+++ src/views/Right.jsx
@@ -1,30 +1,28 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function Right() {
 
   const [products, setProducts] = useState([]);
 
-  useEffect(() => {
+    useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const params = new URLSearchParams({
-          organization_id: '914a1ea0a41f41538a1a489f628d27de',
-          reverse_sort: false,
-          page: 1,
-          size: 10,
-          Appid: '0P3W7TBADIH3K9R',
-          Apikey: '616ca87d3edb4e54bc48ed444168c15f20240712185722233413',
+        const response = await axios.get('/api/products', {
+          params: {
+            organization_id: '914a1ea0a41f41538a1a489f628d27de',
+            reverse_sort: false,
+            page: 1,
+            size: 10,
+            Appid: '0P3W7TBADIH3K9R',
+            Apikey: '616ca87d3edb4e54bc48ed444168c15f20240712185722233413',
+          },
         });
 
-        const response = await fetch(`/api/products?${params}`);
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        setProducts(data.items);
-        console.log(data.items);
+        setProducts(response.data.items);
       } catch (error) {
-        console.error('Error fetching products:', error);
+        console.error('Error fetching products:', error.message, error);
+        setError(error.message);
       }
     };
 
